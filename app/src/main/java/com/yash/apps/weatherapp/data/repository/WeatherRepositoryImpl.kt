c3ab167fa39c1f37dc1dc99c194075c35ebf6a03
yashwanthgajji <gajjiyashwanth7@gmail.com>
2024-12-15 03:48:33 -0500

    handling empty data

--- app/src/main/java/com/yash/apps/weatherapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/yash/apps/weatherapp/data/repository/WeatherRepositoryImpl.kt
@@ -5,14 +5,19 @@ import com.yash.apps.weatherapp.data.remote.dto.CurrentWeatherResponse
 import com.yash.apps.weatherapp.domain.model.CityWeather
 import com.yash.apps.weatherapp.domain.repository.WeatherRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flowOf
 
 class WeatherRepositoryImpl(
     private val weatherApi: WeatherApi
 ): WeatherRepository {
     override suspend fun getCurrentWeather(cityName: String): Flow<CityWeather> {
-        val weatherResponse = weatherApi.getCurrentWeather(cityName = cityName)
-        return flowOf(weatherResponse.convertToCityWeather())
+        try {
+            val weatherResponse = weatherApi.getCurrentWeather(cityName = cityName)
+            return flowOf(weatherResponse.convertToCityWeather())
+        } catch (e: Exception) {
+            return emptyFlow()
+        }
     }
 }
 
